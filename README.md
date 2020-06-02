773365fa3ff69c9d74082a0c3dd9a8582bce48a6
Jose Manuel Corral <jose.manuel.corral@gmail.com>
2020-06-02 13:27:57 +0200

    Fix a lot of things

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # dotfiles
-My dotfiles configuration, for having a fresh development machine 
+My dotfiles configuration, for having a fresh development machine.
+
+# Situation
+
+I Have started to work with WSL2 with windows 10. I have realised that It will be nice to have my common tooling in my daily setup and It will be nice to have all of this setup portable (with this I can setup a development machine in minutes).
+
+# What contains
+
+Here you find a basic interactive installation script.
+It will install:
+
+- Zsh and OhmyZsh (With my plugin setup)
+- netcore 3.1
+- exa (a supercharged ls replacement)
+- nvm (Node version Manager)
+
+
+# How to install
+ Just clone or download this repo and execute ```install.sh```
+
+ Happy Hacking 😎
--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 #first of all
-sudo apt-get install wget curl git
+sudo apt-get install wget curl git unzip git-core zsh
+#Install exa, a modern ls replacement Written in Rust
+curl https://sh.rustup.rs -sSf | sh
+# Till the date of publication of this article, the latest available download version is the 0.8.0
+wget -c https://github.com/ogham/exa/releases/download/v0.8.0/exa-linux-x86_64-0.8.0.zip
+unzip exa-linux-x86_64-0.8.0.zip
+sudo mv exa-linux-x86_64 /usr/local/bin/exa
 #we install sdks and programming languages
+
+#starting with netcore
 wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 sudo dpkg --purge packages-microsoft-prod && sudo dpkg -i packages-microsoft-prod.deb
@@ -10,9 +18,12 @@ sudo apt-get install apt-transport-https
 sudo apt-get update
 sudo apt-get install dotnet-sdk-3.1
 sudo apt-get install aspnetcore-runtime-3.1
-#First we install zsh and git, 
-sudo apt-get install zsh
-sudo apt-get install git-core
+
+#nvm (node version manager)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
 #After that we need to download OhmyZsh wget
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 #and set OhmyZsh as a default shell
